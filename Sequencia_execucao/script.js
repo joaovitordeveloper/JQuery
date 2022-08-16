90fb126fd44ab158bd18423e84de7b8694b76477
João Vitor Pimentel <joao.pimentel@rbmweb.com.br>
2022-08-16 12:56:39 -0300

    subindo arquivos da aula

---
+++ Sequencia_execucao/script.js
@@ -0,0 +1,7 @@
+$(document).ready(function () {
+	//parametro a cima so ira executar o trexo a baixo se todos os elementos da tela ja tiverem sido construidos
+	console.log($('#exemplo'))
+
+
+
+})
---
+++ teste.js
@@ -0,0 +1,6 @@
+$(document).ready(function () {
+
+  var teste = $('#teste');
+  console.log(teste)
+
+})
